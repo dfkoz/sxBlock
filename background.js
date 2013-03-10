066535b2f63b9dfb881cd095e84b684829282131
dfkoz <kozikowski@gmail.com>
2013-03-09 22:20:28 -0500

    Added ability to turn extension on and off with a click.

--- background.js
+++ background.js
@@ -1,10 +1,34 @@
+chrome.browserAction.setBadgeBackgroundColor({color: [0, 50, 200, 150]});
 chrome.browserAction.setBadgeText({text: "ON"});
+var isActive = true;
 
-
+// Process events from tabs.
 chrome.extension.onMessage.addListener(
-  function(msg) {
-  	
-  	if (msg.ct > 0 ) {
-		chrome.browserAction.setBadgeText({text: msg.ct.toString()});	   	
-    }
+ 	function(msg, sender, sendResponse) {
+  		switch(msg.action) {
+  			case 'get_status':
+  				sendResponse({isActive:isActive});
+  				break;
+  			
+  			case 'update_icon':
+  				if (isActive == false) {
+   					chrome.browserAction.setBadgeText({text: "OFF"});
+   					chrome.browserAction.setBadgeBackgroundColor({color: [200, 200, 200, 150]});
+   				} else if (msg.ct > 0 ) {
+					chrome.browserAction.setBadgeText({text: msg.ct.toString()});
+					chrome.browserAction.setBadgeBackgroundColor({color: [0, 50, 200, 150]});	   	
+   				}
+   				
+   				break;			
+  		}
+	}  		 		
+);
+
+// User clicked on icon, which should toggle actice status
+chrome.browserAction.onClicked.addListener(function(activeTab) { 
+	isActive = !isActive;
+	
+	// Ask the tab (via script.js) to update 
+	chrome.tabs.sendMessage(activeTab.id, {action: "toggled", isActive: isActive});
 });
+
--- script.js
+++ script.js
@@ -2,20 +2,52 @@
 var blocked_count = 0;
 
 // Loop through each node that has a data-item-type of 'tweet'
-function scrubSXSW() {
+function scrubSXSW(toggled) {
+	// Get active status from background.js
+	chrome.extension.sendMessage({action:'get_status'}, function(response) { 
 		
-	$('*[data-item-type="tweet"]').each(function (i, d) { 
-		// Does this element have the text sxsw anywhere in it?
-		if ($(d).html().toLowerCase().indexOf('sxsw') > 0) { // Does the content of this tweet contain SXSW?
-			// If so, hide the parent node.
-			$(d).css('display', 'none');
-			blocked_count++;
+		var isActive = response.isActive; 
+		
+		// If we are active, hide offending tweets no matter what. If we are inactive and just changed state,
+		// show the tweets. Otherwise, do nothing.
+		if (isActive) {		
+			$('*[data-item-type="tweet"]').each(function (i, d) { 
+				// Does this element have the text sxsw anywhere in it?
+				if ($(d).html().toLowerCase().indexOf('sxsw') > 0) {
+					// If so, hide or show the parent node.
+					$(d).css('display', 'none');
+					blocked_count++;
+				}
+			});			
+		} else if (!isActive && toggled) {
+			$('*[data-item-type="tweet"]').each(function (i, d) { 
+				// Does this element have the text sxsw anywhere in it?
+				if ($(d).html().toLowerCase().indexOf('sxsw') > 0) {
+					// If so, hide or show the parent node.
+					$(d).css('display', 'list-item');
+				}
+			});						
 		}
+		
+		// Finally, send a message to update the badge.
+		chrome.extension.sendMessage({action:'update_icon', ct: blocked_count});	
 	});
-	
-	chrome.extension.sendMessage({ct: blocked_count});	
 }
+
+
+// Receive messages from background.js, eg, user turned extension off.
+chrome.extension.onMessage.addListener(
+	function(msg) {
+		switch (msg.action) {
+			case 'toggled':
+				scrubSXSW(true); // show or hide annoying tweets before turning off/on		
+				blocked_count = 0; // reset the blocked count.
+				break;		
+		}		
+});
 
 scrubSXSW(); // run once when the page loads.
 $('#timeline').click(scrubSXSW); // and bind an event to the timeline to catch updates.
+
+
 
